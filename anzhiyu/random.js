f25c76b18acc4d12a3ffe6a94ae76e29e8a2976d
atlas <919953051@qq.com>
2024-11-18 11:51:19 +0800

    Site updated: 2024-11-18 11:51:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/17/hello-world/","2024/11/17/集合框架/"];function toRandomPost(){
+var posts=["2024/11/17/hello-world/","2024/11/17/集合框架/","2024/11/18/JVM/","2024/11/18/String/","2024/11/18/动态规划/","2024/11/18/多线程/","2024/11/18/流/","2024/11/18/面试题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
